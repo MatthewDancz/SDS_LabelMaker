c1ecb50f609fbc0a457b00734898001bd0499534
MatthewDancz <danczm@ferris.edu>
2016-07-15 01:59:03 -0400

    Commit 3
    Software Reuse and updates

--- SDS_LabelMaker_Prototype/SDS_LabelMaker_Prototype/BusinessLogic.cs
+++ SDS_LabelMaker_Prototype/SDS_LabelMaker_Prototype/BusinessLogic.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
 
 namespace SDS_LabelMaker_Prototype
 {
     class BusinessLogic
     {
+        DAL newDal = new DAL();
+        SDSLabel newLabel = new SDSLabel();
         public void UpdateLabelText(string s)
         {
 
@@ -17,23 +22,98 @@ namespace SDS_LabelMaker_Prototype
         {
 
         }
+
+        public void UpdateDataBase()
+        {
+            newDal.SaveData(newLabel);
+        }
+
+        public void UpdateLabelData(string s)
+        {
+            newLabel.ChemicalName = s;
+        }
     }
 
     class DAL
     {
-        public void SaveData()
+        private string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"/SDSLabels.xml";
+
+        private string[] nodeNames = new string[]
         {
+            "ChemicalName",
+            "CASRN",
+            "SignalWord",
+            "HazardStatement",
+            //Something for the pictorgrams
+            "ChemicalManufacturer",
+        };
+
+        XmlTextWriter xWriter = null;
+        
+        public void SaveData(SDSLabel l)
+        {
+            if (!File.Exists(path))
+            {
+                createData(l);
+                return;
+            }
+
+            bool ToAdd = shouldLabelBeAdded(l);//Conditional Check for labels existence
+            if (ToAdd)
+            {
+                addSDSLabel(l);
+            }
+            if (!ToAdd)
+            {
+                updateData(l);
+            }
+        }
+
+        public bool shouldLabelBeAdded(SDSLabel l)
+        {
+            return true;
+        }
+
+        private void createData(SDSLabel l)
+        {
+            xWriter = new XmlTextWriter(path, Encoding.UTF8);
+            xWriter.Formatting = Formatting.Indented;
+            xWriter.WriteStartElement("SDSLabels");
+            xWriter.WriteStartElement("Label");
+
+            for (int i = 0; i < nodeNames.Length; i++)
+            {
+                xWriter.WriteStartElement(nodeNames[i]);
+                xWriter.WriteString(l.Properties[i]);
+                xWriter.WriteEndElement();
+            }
+
+            xWriter.WriteEndElement();//End Label Node
+            xWriter.WriteEndElement();//End SDSLabels Node
+            xWriter.Close();
+        }
+
+        private void addSDSLabel(SDSLabel l)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(path);
+            XmlNode label = xDoc.CreateElement("Label");
+            for (int i = 0; i < l.Properties.Length; i++)
+            {
+                XmlNode x = xDoc.CreateElement(nodeNames[i]);
+                x.InnerText = l.Properties[i];
+                label.AppendChild(x);
+            }
+
+            xDoc.DocumentElement.AppendChild(label);
+            xDoc.Save(path);
+            MessageBox.Show("Label data saved.");
 
         }
 
-        private void createData()
+        private void updateData(SDSLabel l)
         {
-
-        }
-
-        private void updateData()
-        {
-
+            
         }
 
         public void RetrieveData()
@@ -45,7 +125,23 @@ namespace SDS_LabelMaker_Prototype
         {
 
         }
+    }
 
+    class SDSLabel
+    {
+        public string[] Properties = null;
+        public string ChemicalName = null;
+        public string CASRN = null;
+        public string SignalWord = null;
+        public string[] HazardStatements = null;
+
+        //Some way to store pictures.
+
+        public string[] ChemicalManufacturer = null;
+    }
+
+    class HazardStatement
+    {
 
     }
 }
--- SDS_LabelMaker_Prototype/SDS_LabelMaker_Prototype/Form1.Designer.cs
+++ SDS_LabelMaker_Prototype/SDS_LabelMaker_Prototype/Form1.Designer.cs
@@ -40,7 +40,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBoxName = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.comboBox4 = new System.Windows.Forms.ComboBox();
@@ -83,7 +83,7 @@
             this.groupBox1.Controls.Add(this.comboBox4);
             this.groupBox1.Controls.Add(this.comboBox3);
             this.groupBox1.Controls.Add(this.comboBox2);
-            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.comboBoxName);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label4);
@@ -202,16 +202,16 @@
             this.label6.TabIndex = 0;
             this.label6.Text = "Pictograms";
             // 
-            // comboBox1
+            // comboBoxName
             // 
-            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.comboBoxName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.comboBox1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(10, 44);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(250, 23);
-            this.comboBox1.TabIndex = 1;
+            this.comboBoxName.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxName.FormattingEnabled = true;
+            this.comboBoxName.Location = new System.Drawing.Point(10, 44);
+            this.comboBoxName.Name = "comboBoxName";
+            this.comboBoxName.Size = new System.Drawing.Size(250, 23);
+            this.comboBoxName.TabIndex = 1;
             // 
             // comboBox2
             // 
@@ -315,7 +315,7 @@
         private System.Windows.Forms.ComboBox comboBox4;
         private System.Windows.Forms.ComboBox comboBox3;
         private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBoxName;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
